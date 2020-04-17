dd635d8e01f89a6e8dcd1be417b3fafa264664bf
Eduardo Caldera Coltters <e.calderac@hotmail.com>
2020-04-17 14:01:07 -0400

    Base para NestJS

---
+++ src/mensajes/mensajes.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Post, Body, Get, Put, Delete } from '@nestjs/common';
+import { CreateMensajeDto } from './dto/create-mensaje-dto';
+
+@Controller('mensajes')
+export class MensajesController {
+
+    @Post()
+    create (@Body() createMensajeDto: CreateMensajeDto){
+        return 'Mensaje creado';
+    }
+
+    @Get()
+    getAll(){
+        return 'Lista de mensajes';
+    }
+
+    @Put(':id')
+    update(@Body() updateMensajeDto: CreateMensajeDto){
+        return 'Mensaje actualizado'
+    }
+
+    @Delete(':id')
+    delete(){
+        return 'Mensaje eliminado'
+    }
+
+}
